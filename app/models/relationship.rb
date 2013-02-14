aba0fc0528b7ea3e93451417da7b1baccc855716
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-14 23:05:11 +0400

    cp

---
+++ app/models/relationship.rb
@@ -0,0 +1,6 @@
+class Relationship < ActiveRecord::Base
+  attr_accessible :user, :hookup, :status, :user_id, :hookup_id
+  
+  belongs_to :user
+  belongs_to :hookup, :class_name => 'User'
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,55 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'rvm/capistrano'                  # Load RVM's capistrano plugin.
+require 'bundler/capistrano'
+require "delayed/recipes"  
+#set :whenever_command, "bundle exec whenever"
+#require "whenever/capistrano"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "in7seconds"
+set :rails_env, 'production'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :rvm_ruby_string, '1.9.3' # Or whatever env you want it to run in.
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+role :web, "174.129.249.0"
+role :db,  "174.129.249.0", :primary => true # This is where Rails migrations will run
+set :delayed_job_server_role, :web
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/home/ubuntu/in7seconds"
+set :deploy_via, :remote_cache
+set :user, "ubuntu"
+set :use_sudo, false
+ssh_options[:forward_agent] = true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# repo details
+set :scm, :git
+set :repository,  "git@github.com:/rromanchuk/in7seconds.git"
+set :branch, "master"
+
+
+after 'deploy:update', 'deploy:cleanup'
+before 'deploy:create_symlink', 'deploy:abort_if_pending_migrations'
+after 'deploy:update_code' do
+  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+end
+before "deploy:restart", "delayed_job:stop"
+after  "deploy:restart", "delayed_job:start"
+after "deploy:stop",  "delayed_job:stop"
+after "deploy:start", "delayed_job:start"
+
+# tasks
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :web, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+  task :abort_if_pending_migrations, :roles => :db, :only => { :primary => true } do
+    run "cd #{release_path} && bundle exec rake RAILS_ENV=#{rails_env} db:abort_if_pending_migrations"
+  end
+  task :nginx, :roles => :web do
+    run 'sudo /opt/nginx/sbin/nginx restart'
+  end
+end
+
