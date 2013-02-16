bd78fb0c478a9e5e012e4b4e5f82cf69ed978206
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-17 02:15:17 +0400

    add exception notifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ In7seconds::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.use ExceptionNotifier,
+  :email_prefix => "[7seconds] ",
+  :sender_address => %{"in7Seconds Exception" <exceptions@in7seconds.com>},
+  :exception_recipients => %w{rromanchuk@gmail.com}
 end
