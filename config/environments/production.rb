96671294bfa4edb0275d6bb0826d2585fa8796d2
Ryan Romanchuk <rromanchuk@gmail.com>
2013-02-17 13:15:08 +0400

    add max to exceptions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ In7seconds::Application.configure do
   config.middleware.use ExceptionNotifier,
   :email_prefix => "[7seconds] ",
   :sender_address => %{"in7Seconds Exception" <exceptions@in7seconds.com>},
-  :exception_recipients => %w{rromanchuk@gmail.com}
+  :exception_recipients => %w{rromanchuk@gmail.com suprmax@gmail.com}
 end
