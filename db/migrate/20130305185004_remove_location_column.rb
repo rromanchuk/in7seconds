6a12ee411c97d70467a3c8e1066df29c4f3eaa1f
Ryan Romanchuk <rromanchuk@gmail.com>
2013-03-05 23:59:29 +0400

    updates

--- db/migrate/20130305185004_remove_location_column.rb
+++ db/migrate/20130305185004_remove_location_column.rb
@@ -1,5 +1,7 @@
 class RemoveLocationColumn < ActiveRecord::Migration
   def change
     remove_column :users, :location
+    remove_column :users, :city
+    remove_column :users, :country
   end
 end
