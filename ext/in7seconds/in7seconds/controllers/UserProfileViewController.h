f5099e3fdc88335a8e88f5e099474ff4136e2a9f
Ryan Romanchuk <rromanchuk@gmail.com>
2013-03-13 01:59:27 +0400

    cleanup

--- ext/in7seconds/in7seconds/controllers/UserProfileViewController.h
+++ ext/in7seconds/in7seconds/controllers/UserProfileViewController.h
@@ -7,21 +7,31 @@
 //
 
 #import "User+REST.h"
+#import "ProfileImageView.h"
 @interface UserProfileViewController : UITableViewController
 @property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
 @property (strong, nonatomic) User *currentUser;
 @property (strong, nonatomic) User *otherUser;
-@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet ProfileImageView *profileImage;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *mutualFriendsHeader;
 @property (weak, nonatomic) IBOutlet UIButton *mutalFriendsButton;
+@property (weak, nonatomic) IBOutlet UILabel *mutualFriendsLabel;
+
 @property (weak, nonatomic) IBOutlet UIButton *mutualGroupsButton;
+@property (weak, nonatomic) IBOutlet UILabel *mutualGroupsHeading;
+@property (weak, nonatomic) IBOutlet UILabel *mutualGroupsLabel;
+
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
-@property (weak, nonatomic) IBOutlet UILabel *friendsLabel;
-@property (weak, nonatomic) IBOutlet UILabel *mutualGroupsLabel;
 
 @property (weak, nonatomic) IBOutlet UILabel *userGroupsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userGroupsHeader;
+
 @property (weak, nonatomic) IBOutlet UILabel *educationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *educationHeader;
 
 
 @property (weak, nonatomic) IBOutlet UITextView *vkUrlTextView;
--- ext/in7seconds/in7seconds/controllers/UserProfileViewController.m
+++ ext/in7seconds/in7seconds/controllers/UserProfileViewController.m
@@ -19,18 +19,33 @@
 {
     [super viewDidLoad];
 
-    self.title = [NSString stringWithFormat:@"%@", self.otherUser.firstName];
+    if (self.otherUser.birthday && [self.otherUser.yearsOld integerValue] > 0) {
+        self.title = [NSString stringWithFormat:@"%@, %@", self.otherUser.firstName, self.otherUser.yearsOld];
+    } else {
+        self.title = [NSString stringWithFormat:@"%@", self.otherUser.lastName];
+    }
     
-    [self.profileImage setImageWithURL:[NSURL URLWithString:self.otherUser.photoUrl]];
+    
+    [self.profileImage setProfilePhotoWithURL:self.otherUser.photoUrl];
     [self.mutalFriendsButton setTitle:[NSString stringWithFormat:@"%@", self.otherUser.mutualFriends] forState:UIControlStateNormal];
     [self.mutualGroupsButton setTitle:[NSString stringWithFormat:@"%@", self.otherUser.mutualGroups] forState:UIControlStateNormal];
     
+    self.mutualFriendsLabel.text = self.otherUser.mutualFriendNames;
+    [self.mutualFriendsLabel sizeToFit];
+
     self.mutualGroupsLabel.text = self.otherUser.mutualGroupNames;
     [self.mutualGroupsLabel sizeToFit];
     
-    self.friendsLabel.text = self.otherUser.mutualFriendNames;
-    [self.friendsLabel sizeToFit];
+    CGRect mgFrame = self.mutualGroupsHeading.frame;
+    mgFrame.origin = CGPointMake(mgFrame.origin.x, (self.mutualFriendsLabel.frame.origin.y + self.mutualFriendsLabel.frame.size.height) + 10);
+    self.mutualGroupsHeading.frame = mgFrame;
     
+    
+    CGRect mglFrame = self.mutualGroupsLabel.frame;
+    mglFrame.origin = CGPointMake(mglFrame.origin.x, (self.mutualGroupsHeading.frame.origin.y + self.mutualGroupsHeading.frame.size.height) + 5);
+    self.mutualGroupsLabel.frame = mglFrame;
+    
+        
     self.userGroupsLabel.text = self.otherUser.groupNames;
     [self.userGroupsLabel sizeToFit];
     
@@ -45,6 +60,43 @@
 
 - (void)back {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)setupMutualFriends {
+//    sampleFilterImages = [[NSMutableSet alloc] init];
+//    int offsetX = 10;
+//    //OstronautFilterType filterType;
+//    for (NSString *filter in self.filters) {
+//        DLog(@"Setting up filter %@", filter);
+//        FilterButtonView *filterButton = [FilterButtonView buttonWithType:UIButtonTypeCustom];
+//        filterButton.frame = CGRectMake(offsetX, 5.0, 50.0, 50.0);
+//        filterButton.filterName = filter;
+//        NSString *filename = [NSString stringWithFormat:@"%@.png", filter];
+//        UIImage *filteredSampleImage  = [UIImage imageNamed:filename];
+//        
+//              
+//        [filterButton setImage:filteredSampleImage forState:UIControlStateNormal];
+//        [filterButton addTarget:self action:@selector(didChangeFilter:) forControlEvents:UIControlEventTouchUpInside];
+//        filterButton.opaque = YES;
+//        filterButton.alpha = 1.0;
+//        [self.filterScrollView addSubview:filterButton];
+//        
+//        UILabel *filterNameLabel = [[UILabel alloc] initWithFrame: CGRectMake(offsetX, filterButton.frame.size.height + 8, filterButton.frame.size.width, 10.0)];
+//        filterNameLabel.text = filter;
+//        filterNameLabel.font = [UIFont fontWithName:@"Helvetica Neue" size:12.0];
+//        filterNameLabel.textAlignment = UITextAlignmentCenter;
+//        filterNameLabel.backgroundColor = [UIColor clearColor];
+//        filterNameLabel.textColor = [UIColor whiteColor];
+//        filterButton.label = filterNameLabel;
+//        [self.filterScrollView addSubview:filterNameLabel];
+//        offsetX += 10 + filterButton.frame.size.width;
+//    }
+//    
+//    DLog(@"number of photos is %d", [sampleFilterImages count]);
+//    //[self saveSampleFilters];
+//    //self.filterScrollView.backgroundColor = [UIColor blueColor];
+//    [self.filterScrollView setContentSize:CGSizeMake(offsetX, 70)];
+    
 }
 
 @end
