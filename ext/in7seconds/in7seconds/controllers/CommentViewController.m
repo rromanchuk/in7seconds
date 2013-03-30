4a791d0fa84d7d5f3c287f38e2e9e1ef90dc7b02
Ryan Romanchuk <rromanchuk@gmail.com>
2013-03-30 17:14:07 +0400

    updates

--- ext/in7seconds/in7seconds/controllers/CommentViewController.m
+++ ext/in7seconds/in7seconds/controllers/CommentViewController.m
@@ -262,7 +262,7 @@
     [self.commentView resignFirstResponder];
     NSString *comment = self.commentView.text;
     if (comment.length == 0 || [comment isEqualToString:NSLocalizedString(@"ENTER_COMMENT", nil)]) {
-        [SVProgressHUD showErrorWithStatus:NSLocalizedString(@"COMMENT_REQUIRED", @"User pressed submit with no comment given")];
+        [SVProgressHUD showErrorWithStatus:NSLocalizedString(@"Упс, ты забыл ввести текст сообщения.", @"User pressed submit with no comment given")];
         return;
     }
     
