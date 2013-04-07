da2702345a77ab09fd5400ca9b2c3e91865d26de
Ryan Romanchuk <rromanchuk@gmail.com>
2013-04-08 01:58:24 +0400

    turn off for forgery protection for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  #protect_from_forgery
+  #skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   helper_method :current_user_json
 
   def current_user_json
