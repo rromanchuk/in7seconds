6b72c2f38593105824ca2ed4f8c144e7b08ecac3
Ryan Romanchuk <rromanchuk@gmail.com>
2013-04-28 16:15:45 +0400

    update

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,5 +1,6 @@
 module Api
   module V1
+    
     class ImagesController < ApplicationController
       before_filter :authenticate_user!
       respond_to :json
@@ -7,9 +8,10 @@ module Api
       def create
         @image = current_user.images.build(params[:image])
         @image.save
-        render json: ''
+        @user = current_user.reload
+        render 'users/authenticate_user'
       end
-      
+
     end
   end
 end
