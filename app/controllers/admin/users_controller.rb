bc6bc18be2db0e433f7394e0a475281f38cbb3c8
Ryan Romanchuk <rromanchuk@gmail.com>
2013-05-10 11:49:47 +0400

    admin panel cleanup remove auth token for web

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,8 +14,6 @@ module Admin
         if current_user.is_requested?(hookup)
           User.fuck(current_user, hookup)
           @user = hookup
-          render 'api/v1/users/match_user'
-          return
         else
           current_user.flirt(hookup)
         end
@@ -23,13 +21,16 @@ module Admin
         redirect_to session[:return_to]
       end
 
-    def reject
-      session[:return_to] ||= request.referer
-      @hookup = User.find(params[:id])
-      current_user.reject(@hookup)
-      
-      redirect_to session[:return_to]
-    end
+      def send_pending_reminder
+        session[:return_to] ||= request.referer
+        hookup = User.find(params[:id])
+        requested = hookup.requested_hookups
+        unless requested.blank?
+          Notification.notify_requested_hookups(hookup, "#{requested.count}")
+        end
+
+        redirect_to session[:return_to]
+      end
 
     end
 end
