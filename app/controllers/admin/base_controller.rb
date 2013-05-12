a1933787f37de416cf9379baa80cd4f4963a17e0
Ryan Romanchuk <rromanchuk@gmail.com>
2013-05-12 18:09:50 +0400

    allow admins from facebook

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,14 +4,16 @@ module Admin
     before_filter :authenticate_user!, :check_admin
     respond_to :html
     ADMINS = [41526347, 2048071, 4807674, 54267159]
-
+    FB_ADMINS = [225311, 7300784]
+    
     def check_admin
-      if ADMINS.include?(current_user.vkuid)
+      if ADMINS.include?(current_user.vkuid) or FB_ADMINS.include?(current_user.fbuid)
 
       else
         redirect_to root_path
         return
       end
     end
+    
   end
 end
