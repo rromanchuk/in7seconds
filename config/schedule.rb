0d08247fecee18cdc6181020fc3b49ff45b8d9fa
Ryan Romanchuk <rromanchuk@gmail.com>
2013-05-15 21:25:52 +0400

    add scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 
-every 1.day, :at => '4:30 am' do
+every 5.day, :at => '4:30 am' do
   runner "User.update_friends"
+end
+
+every 1.day, :at => '8:30 am' do
+  runner "Relationship.notify_requested_hookups"
 end
 
 every 1.day, at: '8:00 am' do
