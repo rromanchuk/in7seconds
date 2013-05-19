9ea76c2ee9e6bbf4e371bec577d9e7348d5ab796
Ryan Romanchuk <rromanchuk@gmail.com>
2013-05-19 18:12:54 +0400

    add cuchi as admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,7 +4,7 @@ module Admin
     before_filter :authenticate_user!, :check_admin
     respond_to :html
     ADMINS = [41526347, 2048071, 4807674, 54267159]
-    FB_ADMINS = [225311, 7300784]
+    FB_ADMINS = [225311, 7300784, 1071841049]
     
     def check_admin
       if current_user && (ADMINS.include?(current_user.vkuid) or FB_ADMINS.include?(current_user.fbuid))
