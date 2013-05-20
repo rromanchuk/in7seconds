8f2dc4a89a271e64c9b91d8020ec6cded1f0c724
Ryan Romanchuk <rromanchuk@gmail.com>
2013-05-20 11:03:48 +0400

    work on notification banner

---
+++ ext/in7seconds/in7seconds/views/NotificationBanner.h
@@ -0,0 +1,22 @@
+//
+//  NotificationBanner.h
+//  Ostronaut
+//
+//  Created by Ryan Romanchuk on 11/30/12.
+//
+//
+
+
+@interface NotificationBanner : UIView
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UILabel *notificationTextLabel;
+@property (strong, nonatomic) IBOutlet UIButton *dismissButton;
+
+
+@property (strong, nonatomic) User *user;
+@property (strong, nonatomic) id sender;
+
+@property (strong, nonatomic) NSString *segueTo;
+- (void)setupView;
+
+@end
