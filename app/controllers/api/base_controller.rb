47a66f451d03f024c836ddd7019c8340aa74b4dd
Ryan Romanchuk <rromanchuk@gmail.com>
2013-08-18 14:41:40 +0400

    fix

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,7 +1,6 @@
 module Api
   class BaseController < ActionController::Base
     
-
     skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
     before_filter :miniprofile
 
@@ -22,19 +21,6 @@ module Api
       message = {:channel => channel, :data => data, :ext => {:auth_token => FAYE_TOKEN}}
       uri = URI.parse(CONFIG[:faye_host])
       Net::HTTP.post_form(uri, :message => message.to_json)
-    end
-
-    def render_error(status, exception)
-      ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
-      respond_to do |format|
-        format.html { render template: "pages/error_#{status}", layout: 'layouts/splash', status: status }
-        format.all { render nothing: true, status: status }
-      end
-    end 
-
-    unless Rails.application.config.consider_all_requests_local
-      rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-      rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
     end
   end
 end
