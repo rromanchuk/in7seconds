426ef0f4c96963063cab352a0ca97d9f6942ba7c
Ryan Romanchuk <rromanchuk@gmail.com>
2013-08-18 20:25:37 +0400

    uploaded flag

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -8,6 +8,7 @@ module Api
       def create
         @image = current_user.images.build(params[:image])
         @image.provider = :phone
+        @image.is_uploaded = true
         @image.save
         @user = current_user.reload
         render 'api/v1/users/authenticated_user'
